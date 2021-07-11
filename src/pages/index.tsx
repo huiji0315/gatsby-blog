30a5ebdf3c7fc6f8362f7850dd7df16be5eae1fe
huiji0315 <huiji0315@gmail.com>
2021-07-11 19:21:46 +0900

    메인 페이지에서 Props로 받아 포스트 데이터 출력하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,16 @@ import GlobalStyle from 'components/Common/GlobalStyle';
 import Footer from 'components/Common/Footer';
 import CategoryList from 'components/Main/CategoryList';
 import Introduction from 'components/Main/Introduction';
-import PostList from 'components/Main/PostList';
+import PostList, { PostType } from 'components/Main/PostList';
+import { graphql } from 'gatsby';
+
+interface IndexPageProps {
+  data: {
+    allMarkdownRemark: {
+      edges: PostType[];
+    };
+  };
+}
 
 const CATEGORY_LIST = {
   All: 5,
@@ -15,19 +24,48 @@ const CATEGORY_LIST = {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100%;
 `;
 
-const IndexPage: FunctionComponent = function () {
+// 쿼리를 통해 받은 데이터는 data라는 이름의 Props에 담겨있다.
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-      <PostList />
+      <PostList posts={edges} />
       <Footer />
     </Container>
   );
 };
 
 export default IndexPage;
+
+// 필요한 데이터를 받아오기 위한 쿼리
+export const queryPostList = graphql`
+  query queryPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`;
