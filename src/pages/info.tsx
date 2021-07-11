d5a28349ffdaa1d9542c2de9c92795a4d47e349e
huiji0315 <huiji0315@gmail.com>
2021-07-11 11:46:04 +0900

    EmotionJS 라이브러리를 통한 컴포넌트 스타일링 방법

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,39 @@
 import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`;
+
+// Tagged Template Literal 방식을 통해 정의한 CSS 적용 방법
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`;
+
+// Tagged Template Literal 방식을 통한 Styled Component 생성 방법
+const Text1 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`;
+
+// 객체를 통한 Styled Component 생성 방법
+// Camel Case 적용 && 스타일 값은 무조건 String Type으로 전달
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}));
 
 interface InfoPageProps {
   data: {
@@ -22,7 +56,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      {title} {description} {author}
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={true}>{author}</Text2>
     </div>
   );
 };
