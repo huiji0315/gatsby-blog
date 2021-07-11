4017b87809f9d2616f075e78600d61938c64e702
huiji0315 <huiji0315@gmail.com>
2021-07-11 22:39:41 +0900

    gatsby-image 라이브러리 사용

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,24 +1,27 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
+import Img, { FluidObject } from 'gatsby-image';
 
-// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
-const PROFILE_IMAGE_LINK =
-  'https://avatars.githubusercontent.com/u/41608951?v=4';
+export interface ProfileImageProps {
+  profileImage: FluidObject;
+}
 
-  const ProfileImageWrapper = styled.img`
-    width: 120px;
-    height: 120px;
-    margin-bottom: 30px;
-    border-radius: 50%;
-  
-    @media (max-width: 768px) {
-      width: 80px;
-      height: 80px;
-    }
-  `; // 반응형 디자인 -> 브라우저 가로 사이즈가 태블릿 기준 너비인 768px 이하인 경우에는 80px로 크기 줄여주기
+const ProfileImageWrapper = styled(Img)`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
 
-const ProfileImage: FunctionComponent = function () {
-  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />;
+  @media (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
+`;
+
+const ProfileImage: FunctionComponent<ProfileImageProps> = function ({
+  profileImage,
+}) {
+  return <ProfileImageWrapper fluid={profileImage} alt="Profile Image" />;
 };
 
 export default ProfileImage;
